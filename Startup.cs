6d01659e21ab8dbbc30e12c40d19ec6cd8d51fa9
Jaskirat KAUR <19kirat@gmail.com>
2022-03-27 19:51:42 -0400

    updated the startup file with AddRAzorPages services , app Authentication and endpoints to map Razor pages.

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace UserAuthenticationApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,8 @@ namespace UserAuthenticationApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +54,7 @@ namespace UserAuthenticationApp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();   
             });
         }
     }
